d89d90f80ea9447235b024891d233a1ba9a95a19
Dario Castellano <dariocastellano18@gmail.com>
2021-02-16 17:06:04 +0100

    webapp fix base

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -11,8 +11,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "78531a3fd234dc985b144608e84bd61a",
-"/": "78531a3fd234dc985b144608e84bd61a",
+"index.html": "0857718016258c4c61085f26fae3389a",
+"/": "0857718016258c4c61085f26fae3389a",
 "main.dart.js": "b234a8afa5626ec05a31996bddbdd69a",
 "manifest.json": "2b2ac4d8c3d8a70bdd5757e2a836950e",
 "version.json": "9290e178eaec55d4cba23549e896b2d3"
